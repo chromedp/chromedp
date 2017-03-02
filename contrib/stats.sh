8c9e75e5abd06831860bef0006408cc9b3074619
Kenneth Shaw <kenneth.shaw@knq.io>
2017-03-02 11:27:43 +0700

    Add kb dir to contrib/stats.sh

--- contrib/stats.sh
+++ contrib/stats.sh
@@ -3,7 +3,7 @@
 SRC=$(realpath $(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../)
 
 FILES=$(find $SRC/cmd/chromedp-gen -type f -iname \*.go -not -iname \*.qtpl.go -print0|wc -l --files0-from=-|head -n -1)$'\n'
-FILES+=$(find $SRC/{client,runner,contrib,examples} -type f -iname \*.go -print0|wc -l --files0-from=-|head -n -1)$'\n'
+FILES+=$(find $SRC/{client,runner,kb,contrib,examples} -type f -iname \*.go -print0|wc -l --files0-from=-|head -n -1)$'\n'
 FILES+=$(find $SRC/ -maxdepth 1 -type f -iname \*.go -print0|wc -l --files0-from=-|head -n -1)
 
 AUTOG=$(find $SRC/cdp/ -type f -iname \*.go -not -iname \*easyjson\* -print0|wc -l --files0-from=-|head -n -1)
