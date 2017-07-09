a4589d95d6795227ec34c345d068f87cd16c8244
Kenneth Shaw <kenneth.shaw@knq.io>
2017-07-09 08:44:09 +0700

    Fixing Timestamp type name in cookie example

--- examples/cookie/main.go
+++ examples/cookie/main.go
@@ -71,7 +71,7 @@ func main() {
 func setcookies(host string, res *string) cdp.Tasks {
 	return cdp.Tasks{
 		cdp.ActionFunc(func(ctxt context.Context, h cdptypes.Handler) error {
-			expr := cdptypes.Timestamp(time.Now().Add(180 * 24 * time.Hour))
+			expr := cdptypes.TimeSinceEpoch(time.Now().Add(180 * 24 * time.Hour))
 			success, err := network.SetCookie(host, "cookiename", "cookievalue").
 				WithExpirationDate(&expr).
 				WithDomain("localhost").
