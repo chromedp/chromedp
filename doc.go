9424dc57d6f066b60b2d1d1af2e265b09f027315
Kenneth Shaw <kenneth.shaw@knq.io>
2017-12-22 10:14:21 +0700

    Removing empty doc.go file

--- doc.go
+++
@@ -1,7 +0,0 @@
-// Package chromedp is a high level Chrome Debugging Protocol domain manager
-// that simplifies driving web browsers (Chrome, Safari, Edge, Android Web
-// Views, and others) for scraping, unit testing, or profiling web pages.
-//
-// chromedp requires no third-party dependencies (ie, Selenium), implementing
-// the async Chrome Debugging Protocol natively.
-package chromedp
