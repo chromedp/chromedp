117d42c3dd4faa94aae004405000ab7dad42cbbd
Kenneth Shaw <kenneth.shaw@knq.io>
2018-03-20 19:07:54 +0700

    Minor change to client (supporting 1.3)

--- client/client.go
+++ client/client.go
@@ -184,7 +184,7 @@ func (c *Client) newTarget(ctxt context.Context, buf []byte) (Target, error) {
 		return nil, err
 	}
 
-	if ver != "1.1" && ver != "1.2" {
+	if ver != "1.1" && ver != "1.2" && ver != "1.3" {
 		return nil, ErrUnsupportedProtocolVersion
 	}
 
