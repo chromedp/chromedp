4e797cb12423f95c2e1f87c14eb60d4220173976
Daniel Martí <mvdan@mvdan.cc>
2019-04-29 17:39:00 +0700

    rewrite ExampleTitle to not require internet
    And now it can be run as part of 'go test'.

--- example_test.go
+++ example_test.go
@@ -4,31 +4,52 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/chromedp/chromedp"
 )
+
+func writeHTML(content string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, strings.TrimSpace(content))
+	})
+}
 
 func ExampleTitle() {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
+	ts := httptest.NewServer(writeHTML(`
+<html>
+<head>
+	<title>fancy website title</title>
+</head>
+<body>
+	<div id="loaded"></div>
+</body>
+</html>
+	`))
+	defer ts.Close()
+
 	var title string
 	if err := chromedp.Run(ctx,
-		chromedp.Navigate("https://github.com/chromedp/chromedp/issues"),
-		chromedp.WaitVisible("#start-of-content", chromedp.ByID),
+		chromedp.Navigate(ts.URL),
+		chromedp.WaitVisible("#loaded", chromedp.ByID),
 		chromedp.Title(&title),
 	); err != nil {
 		panic(err)
 	}
-
 	fmt.Println(title)
 
-	// no expected output, to not run this test as part of 'go test'; it's
-	// too slow, requiring internet access.
+	// Output:
+	// fancy website title
 }
 
 func ExampleExecAllocator() {
