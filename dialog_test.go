6278efe4b8a9a386ff6d1c8d0e9865be4efd87be
Randy Cahyana <ranren.cahyana@gmail.com>
2019-04-30 13:43:50 +0700

    dialog: add more tests
    This add more test to cover alert, prompt, and confirm
    dialog.

--- dialog_test.go
+++ dialog_test.go
@@ -9,26 +9,146 @@ import (
 func TestCloseDialog(t *testing.T) {
 	t.Parallel()
 
-	t.Run("Alert", func(t *testing.T) {
-		ctx, cancel := testAllocate(t, "")
-		defer cancel()
+	tests := []struct {
+		name       string
+		accept     bool
+		promptText string
+		dialogType page.DialogType
+		sel        string
+		want       string
+	}{
+		{
+			name:       "AlertAcceptWithPromptText",
+			accept:     true,
+			promptText: "this is a prompt text",
+			dialogType: page.DialogTypeAlert,
+			sel:        "#alert",
+			want:       "alert text",
+		},
+		{
+			name:       "AlertDismissWithPromptText",
+			accept:     false,
+			promptText: "this is a prompt text",
+			dialogType: page.DialogTypeAlert,
+			sel:        "#alert",
+			want:       "alert text",
+		},
+		{
+			name:       "AlertAcceptWithoutPromptText",
+			accept:     true,
+			dialogType: page.DialogTypeAlert,
+			sel:        "#alert",
+			want:       "alert text",
+		},
+		{
+			name:       "AlertDismissWithoutPromptText",
+			accept:     false,
+			dialogType: page.DialogTypeAlert,
+			sel:        "#alert",
+			want:       "alert text",
+		},
+		{
+			name:       "PromptAcceptWithPromptText",
+			accept:     true,
+			promptText: "this is a prompt text",
+			dialogType: page.DialogTypePrompt,
+			sel:        "#prompt",
+			want:       "prompt text",
+		},
+		{
+			name:       "PromptDismissWithPromptText",
+			accept:     false,
+			promptText: "this is a prompt text",
+			dialogType: page.DialogTypePrompt,
+			sel:        "#prompt",
+			want:       "prompt text",
+		},
+		{
+			name:       "PromptAcceptWithoutPromptText",
+			accept:     true,
+			dialogType: page.DialogTypePrompt,
+			sel:        "#prompt",
+			want:       "prompt text",
+		},
+		{
+			name:       "PromptDismissWithoutPromptText",
+			accept:     false,
+			dialogType: page.DialogTypePrompt,
+			sel:        "#prompt",
+			want:       "prompt text",
+		},
+		{
+			name:       "ConfirmAcceptWithPromptText",
+			accept:     true,
+			promptText: "this is a prompt text",
+			dialogType: page.DialogTypeConfirm,
+			sel:        "#confirm",
+			want:       "confirm text",
+		},
+		{
+			name:       "ConfirmDismissWithPromptText",
+			accept:     false,
+			promptText: "this is a prompt text",
+			dialogType: page.DialogTypeConfirm,
+			sel:        "#confirm",
+			want:       "confirm text",
+		},
+		{
+			name:       "ConfirmAcceptWithoutPromptText",
+			accept:     true,
+			dialogType: page.DialogTypeConfirm,
+			sel:        "#confirm",
+			want:       "confirm text",
+		},
+		{
+			name:       "ConfirmDismissWithoutPromptText",
+			accept:     false,
+			dialogType: page.DialogTypeConfirm,
+			sel:        "#confirm",
+			want:       "confirm text",
+		},
+	}
 
-		ListenTarget(ctx, func(ev interface{}) {
-			switch ev.(type) {
-			case *page.EventJavascriptDialogOpening:
-				if err := Run(ctx,
-					page.HandleJavaScriptDialog(true),
-				); err != nil {
-					t.Error(err)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx, cancel := testAllocate(t, "")
+			defer cancel()
+
+			ListenTarget(ctx, func(ev interface{}) {
+				switch e := ev.(type) {
+				case *page.EventJavascriptDialogOpening:
+					if e.Type != test.dialogType {
+						t.Errorf("expected dialog type to be '%s', got: '%s'", test.dialogType, e.Type)
+					}
+					if e.Message != test.want {
+						t.Errorf("expected dialog message to be '%s', got: '%s'", test.want, e.Message)
+					}
+
+					task := page.HandleJavaScriptDialog(test.accept)
+					if test.promptText != "" {
+						task = task.WithPromptText(test.promptText)
+					}
+					if err := Run(ctx, task); err != nil {
+						t.Error(err)
+					}
+				case *page.EventJavascriptDialogClosed:
+					if e.Result != test.accept {
+						t.Errorf("expected result to be %t, got %t", test.accept, e.Result)
+					}
+					if e.UserInput != test.promptText {
+						t.Errorf("expected user input to be %q, got %q", test.promptText, e.UserInput)
+					}
 				}
+			})
+
+			if err := Run(ctx,
+				Navigate(testdataDir+"/dialog.html"),
+				Click(test.sel, ByID, NodeVisible),
+			); err != nil {
+				t.Fatalf("got error on DialogText %v", err)
 			}
 		})
-
-		if err := Run(ctx,
-			Navigate(testdataDir+"/dialog.html"),
-			Click("#alert", ByID, NodeVisible),
-		); err != nil {
-			t.Fatalf("got error on DialogText %v", err)
-		}
-	})
+	}
 }
