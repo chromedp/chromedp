chromedp/chromedp

commit dc091867befd3c271be0e0d1e4b48335ee40af0b

replace gorilla/websocket with gobwas/ws
Our websocket code never showed up in the CPU profiles, so it's not
surprising that performance isn't affected.

However, gobwas/ws does generate significantly less garbage, as can be
seen in the allocs/op improvement. This is mainly because it lets us use
lower-level APIs, reusing more memory.

The large improvement in alloc/op is mainly because we dropped the large
read and write buffer sizes. In gobwas/ws, these buffers are only used
for the initial http connection, so they don't matter for the actual
websocket connection after HTTP. The default is just 4KiB, which is more
than enough, and significantly reduces the memory allocated.

name           old time/op    new time/op    delta
TabNavigate-8    29.6ms ± 5%    29.7ms ± 4%     ~     (p=0.818 n=6+6)

name           old alloc/op   new alloc/op   delta
TabNavigate-8     529kB ± 0%     162kB ± 2%  -69.41%  (p=0.002 n=6+6)

name           old allocs/op  new allocs/op  delta
TabNavigate-8     1.35k ± 2%     1.26k ± 3%   -6.09%  (p=0.002 n=6+6)

conn.go
@@ -1,49 +1,61 @@
-// +build !nhooyr !gobwas
-
 package chromedp
 
 import (
 	"bytes"
 	"context"
 	"io"
+	"net"
 
 	"github.com/chromedp/cdproto"
-	"github.com/gorilla/websocket"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 	"github.com/mailru/easyjson"
 	"github.com/mailru/easyjson/jlexer"
 	"github.com/mailru/easyjson/jwriter"
 )
 
-// Conn wraps a gorilla/websocket.Conn connection.
+// Transport is the common interface to send/receive messages to a target.
+type Transport interface {
+	Read(context.Context, *cdproto.Message) error
+	Write(context.Context, *cdproto.Message) error
+	io.Closer
+}
+
+// Conn implements Transport with a gobwas/ws websocket connection.
 type Conn struct {
-	conn *websocket.Conn
+	conn net.Conn
 
 	// buf helps us reuse space when reading from the websocket.
 	buf bytes.Buffer
 
+	// reuse the websocket reader and writer to avoid an alloc per
+	// Read/Write.
+	reader wsutil.Reader
+	writer wsutil.Writer
+
 	// reuse the easyjson structs to avoid allocs per Read/Write.
-	lexer  jlexer.Lexer
-	writer jwriter.Writer
+	decoder jlexer.Lexer
+	encoder jwriter.Writer
 
 	dbgf func(string, ...interface{})
 }
 
-// DialContext dials the specified websocket URL using gorilla/websocket.
+// DialContext dials the specified websocket URL using gobwas/ws.
 func DialContext(ctx context.Context, urlstr string, opts ...DialOption) (*Conn, error) {
-	d := &websocket.Dialer{
-		ReadBufferSize:  25 * 1024 * 1024,
-		WriteBufferSize: 10 * 1024 * 1024,
-	}
-
 	// connect
-	conn, _, err := d.DialContext(ctx, urlstr, nil)
+	conn, br, _, err := ws.Dial(ctx, urlstr)
 	if err != nil {
 		return nil, err
 	}
+	if br != nil {
+		panic("br should not be nil")
+	}
+	// ws.PutReader(br)
 
 	// apply opts
 	c := &Conn{
-		conn: conn,
+		conn:   conn,
+		writer: *wsutil.NewWriterBufferSize(conn, ws.StateClientSide, ws.OpText, 4096),
 	}
 	for _, o := range opts {
 		o(c)
@@ -72,18 +84,19 @@ func unmarshal(lex *jlexer.Lexer, data []byte, v easyjson.Unmarshaler) error {
 // Read reads the next message.
 func (c *Conn) Read(_ context.Context, msg *cdproto.Message) error {
 	// get websocket reader
-	typ, r, err := c.conn.NextReader()
+	c.reader = wsutil.Reader{Source: c.conn, State: ws.StateClientSide}
+	h, err := c.reader.NextFrame()
 	if err != nil {
 		return err
 	}
-	if typ != websocket.TextMessage {
+	if h.OpCode != ws.OpText {
 		return ErrInvalidWebsocketMessage
 	}
 
 	// Unmarshal via a bytes.Buffer. Don't use UnmarshalFromReader, as that
 	// uses ioutil.ReadAll, which uses a brand new bytes.Buffer each time.
 	// That doesn't reuse any space.
-	buf, err := c.bufReadAll(r)
+	buf, err := c.bufReadAll(&c.reader)
 	if err != nil {
 		return err
 	}
@@ -92,7 +105,7 @@ func (c *Conn) Read(_ context.Context, msg *cdproto.Message) error {
 	}
 
 	// Reuse the easyjson lexer.
-	if err := unmarshal(&c.lexer, buf, msg); err != nil {
+	if err := unmarshal(&c.decoder, buf, msg); err != nil {
 		return err
 	}
 
@@ -106,35 +119,31 @@ func (c *Conn) Read(_ context.Context, msg *cdproto.Message) error {
 
 // Write writes a message.
 func (c *Conn) Write(_ context.Context, msg *cdproto.Message) error {
-	w, err := c.conn.NextWriter(websocket.TextMessage)
-	if err != nil {
-		return err
-	}
-	defer w.Close()
+	c.writer.Reset(c.conn, ws.StateClientSide, ws.OpText)
 
 	// Reuse the easyjson writer.
-	c.writer = jwriter.Writer{}
+	c.encoder = jwriter.Writer{}
 
 	// Perform the marshal.
-	msg.MarshalEasyJSON(&c.writer)
-	if err := c.writer.Error; err != nil {
+	msg.MarshalEasyJSON(&c.encoder)
+	if err := c.encoder.Error; err != nil {
 		return err
 	}
 
 	// Write the bytes to the websocket.
 	// BuildBytes consumes the buffer, so we can't use it as well as DumpTo.
 	if c.dbgf != nil {
-		buf, _ := c.writer.BuildBytes()
+		buf, _ := c.encoder.BuildBytes()
 		c.dbgf("-> %s", buf)
-		if _, err := w.Write(buf); err != nil {
+		if _, err := c.writer.Write(buf); err != nil {
 			return err
 		}
 	} else {
-		if _, err := c.writer.DumpTo(w); err != nil {
+		if _, err := c.encoder.DumpTo(&c.writer); err != nil {
 			return err
 		}
 	}
-	return w.Close()
+	return c.writer.Flush()
 }
 
 // DialOption is a dial option.

chromedp_test.go
@@ -446,7 +446,7 @@ func TestDialTimeout(t *testing.T) {
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 		_, err = NewBrowser(ctx, url, WithDialTimeout(0))
-		got, want := fmt.Sprintf("%v", err), "unexpected EOF"
+		got, want := fmt.Sprintf("%v", err), "EOF"
 		if !strings.Contains(got, want) {
 			t.Fatalf("got %q, want %q", got, want)
 		}

browser.go
@@ -3,7 +3,6 @@ package chromedp
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
@@ -69,13 +68,6 @@ type Browser struct {
 
 // TODO: move Transport to the same file as its default implementation once we
 // settle on one websocket library.
-
-// Transport is the common interface to send/receive messages to a target.
-type Transport interface {
-	Read(context.Context, *cdproto.Message) error
-	Write(context.Context, *cdproto.Message) error
-	io.Closer
-}
 
 type cmdJob struct {
 	// msg is the message being sent.

go.mod
@@ -4,6 +4,9 @@ go 1.11
 
 require (
 	github.com/chromedp/cdproto v0.0.0-20190429085128-1aa4f57ff2a9
-	github.com/gorilla/websocket v1.4.0
+	github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee // indirect
+	github.com/gobwas/pool v0.2.0 // indirect
+	github.com/gobwas/ws v1.0.0
 	github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983
+	golang.org/x/sys v0.0.0-20190509141414-a5b02f93d862 // indirect
 )

go.sum
@@ -1,9 +1,15 @@
 github.com/chromedp/cdproto v0.0.0-20190429085128-1aa4f57ff2a9 h1:ARnDd2vEk91rLNra8yk1hF40H8z+1HrD6juNpe7FsI0=
 github.com/chromedp/cdproto v0.0.0-20190429085128-1aa4f57ff2a9/go.mod h1:xquOK9dIGFlLaIGI4c6IyfLI/Gz0LiYYuJtzhsUODgI=
-github.com/gorilla/websocket v1.4.0 h1:WDFjx/TMzVgy9VdMMQi2K2Emtwi2QcUQsztZ/zLaH/Q=
-github.com/gorilla/websocket v1.4.0/go.mod h1:E7qHFY5m1UJ88s3WnNqhKjPHQ0heANvMoAMk2YaljkQ=
+github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee h1:s+21KNqlpePfkah2I+gwHF8xmJWRjooY+5248k6m4A0=
+github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee/go.mod h1:L0fX3K22YWvt/FAX9NnzrNzcI4wNYi9Yku4O0LKYflo=
+github.com/gobwas/pool v0.2.0 h1:QEmUOlnSjWtnpRGHF3SauEiOsy82Cup83Vf2LcMlnc8=
+github.com/gobwas/pool v0.2.0/go.mod h1:q8bcK0KcYlCgd9e7WYLm9LpyS+YeLd8JVDW6WezmKEw=
+github.com/gobwas/ws v1.0.0 h1:1WdyfgUcImUfVBvYbsW2krIsnko+1QU2t45soaF8v1M=
+github.com/gobwas/ws v1.0.0/go.mod h1:szmBTxLgaFppYjEmNtny/v3w89xOydFnnZMcgRRu/EM=
 github.com/knq/sysutil v0.0.0-20181215143952-f05b59f0f307 h1:vl4eIlySbjertFaNwiMjXsGrFVK25aOWLq7n+3gh2ls=
 github.com/knq/sysutil v0.0.0-20181215143952-f05b59f0f307/go.mod h1:BjPj+aVjl9FW/cCGiF3nGh5v+9Gd3VCgBQbod/GlMaQ=
 github.com/mailru/easyjson v0.0.0-20180823135443-60711f1a8329/go.mod h1:C1wdFJiN94OJF2b5HbByQZoLdCWB1Yqtg26g4irojpc=
 github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983 h1:wL11wNW7dhKIcRCHSm4sHKPWz0tt4mwBsVodG7+Xyqg=
 github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983/go.mod h1:C1wdFJiN94OJF2b5HbByQZoLdCWB1Yqtg26g4irojpc=
+golang.org/x/sys v0.0.0-20190509141414-a5b02f93d862 h1:rM0ROo5vb9AdYJi1110yjWGMej9ITfKddS89P3Fkhug=
+golang.org/x/sys v0.0.0-20190509141414-a5b02f93d862/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=

commit 015708072eadb724b6ef5d648f6eab2bfae88f1c

increase the websocket write buffer size to 1MiB
See the comment on wsWriteBufferSize for context. This somewhat fixes
the regression in dc091867be, as we went from a 25MiB message size limit
to a 4KiB one. 1MiB seems like a reasonable middle ground for the time
being, keeping the memory usage low.

Fixes #395.

conn.go
@@ -40,6 +40,19 @@ type Conn struct {
 	dbgf func(string, ...interface{})
 }
 
+// Chrome doesn't support fragmentation of incoming websocket messages. To
+// compensate this, they support single-fragment messages of up to 100MiB.
+//
+// If our write buffer size is too small, large messages will get fragmented,
+// and Chrome will silently crash. And if it's too large, chromedp will require
+// more memory for all users.
+//
+// For now, make this a middle ground. 1MiB is large enough for practically any
+// outgoing message, but small enough to not take too much meomry.
+//
+// See https://github.com/ChromeDevTools/devtools-protocol/issues/175.
+const wsWriteBufferSize = 1 << 20
+
 // DialContext dials the specified websocket URL using gobwas/ws.
 func DialContext(ctx context.Context, urlstr string, opts ...DialOption) (*Conn, error) {
 	// connect
@@ -53,8 +66,9 @@ func DialContext(ctx context.Context, urlstr string, opts ...DialOption) (*Conn,
 
 	// apply opts
 	c := &Conn{
-		conn:   conn,
-		writer: *wsutil.NewWriterBufferSize(conn, ws.StateClientSide, ws.OpText, 4096),
+		conn: conn,
+		writer: *wsutil.NewWriterBufferSize(conn,
+			ws.StateClientSide, ws.OpText, wsWriteBufferSize),
 	}
 	for _, o := range opts {
 		o(c)

chromedp_test.go
@@ -550,3 +550,17 @@ func TestLogOptions(t *testing.T) {
 		}
 	}
 }
+
+func TestLargeOutboundMessages(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := testAllocate(t, "")
+	defer cancel()
+
+	// ~50KiB of JS should fit just fine in our current buffer of 1MiB.
+	expr := fmt.Sprintf("//%s\n", strings.Repeat("x", 50 << 10))
+	res := new([]byte)
+	if err := Run(ctx, Evaluate(expr, res)); err != nil {
+		t.Fatal(err)
+	}
+}