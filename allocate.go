9824760f81ac2d7154fae173f7fd7502dc4ed654
Daniel Martí <mvdan@mvdan.cc>
2020-01-12 16:02:23 +0800

    double the websocket url read timeout
    We hit this error pretty regularly on CI, presumably because the machine
    is under stress.
    
    Try doubling the timeout. After all, it's only meant to stop blocking
    forever.

--- allocate.go
+++ allocate.go
@@ -215,7 +215,7 @@ func (a *ExecAllocator) Allocate(ctx context.Context, opts ...BrowserOption) (*B
 	// Chrome will sometimes fail to print the websocket, or run for a long
 	// time, without properly exiting. To avoid blocking forever in those
 	// cases, give up after ten seconds.
-	const wsURLReadTimeout = 10 * time.Second
+	const wsURLReadTimeout = 20 * time.Second
 
 	var wsURL string
 	wsURLChan := make(chan struct{}, 1)
