7073d01b91bb7b996712bc19d630445bd460135f
David Cuadrado <dcuadrado@truora.com>
2020-12-02 19:30:44 -0500

    Do not set pdeathsig to the process on Lambda
    Since it's not supported by the AWS Lambda runtime
    
    Fixes #562

--- allocate_linux.go
+++ allocate_linux.go
@@ -3,11 +3,18 @@
 package chromedp
 
 import (
+	"os"
 	"os/exec"
 	"syscall"
 )
 
 func allocateCmdOptions(cmd *exec.Cmd) {
+	_, isLambda := os.LookupEnv("LAMBDA_TASK_ROOT")
+	if isLambda {
+		// do nothing on AWS Lambda
+		return
+	}
+
 	if cmd.SysProcAttr == nil {
 		cmd.SysProcAttr = new(syscall.SysProcAttr)
 	}
