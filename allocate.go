3f81ff8463df931d8f1c2574610b62660c06a01d
Zeke Lu <lvzecai@gmail.com>
2021-08-27 06:06:29 +0800

    mention that --disable-gpu is still required in some cases

--- allocate.go
+++ allocate.go
@@ -473,11 +473,13 @@ func Headless(a *ExecAllocator) {
 
 // DisableGPU is the command line option to disable the GPU process.
 //
-// The --disable-gpu option is a temporary work around for a few bugs
-// in headless mode. But now it's not longer required. References:
+// The --disable-gpu option is a temporary workaround for a few bugs
+// in headless mode. According to the references below, it's no longer required:
 // - https://bugs.chromium.org/p/chromium/issues/detail?id=737678
 // - https://github.com/puppeteer/puppeteer/pull/2908
 // - https://github.com/puppeteer/puppeteer/pull/4523
+// But according to this reported issue, it's still required in some cases:
+// - https://github.com/chromedp/chromedp/issues/904
 func DisableGPU(a *ExecAllocator) {
 	Flag("disable-gpu", true)(a)
 }
