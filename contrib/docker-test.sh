80f1283928b827ccb5ba0e4dca6e94c398d53f73
Kenneth Shaw <kenshaw@gmail.com>
2022-01-06 07:37:32 +0700

    Adding environment variable to control which headless-shell image is used in contrib/docker-test.sh

--- contrib/docker-test.sh
+++ contrib/docker-test.sh
@@ -4,6 +4,8 @@ SRC=$(realpath $(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)/..)
 
 pushd $SRC &> /dev/null
 
+IMAGE=${IMAGE:-chromedp/headless-shell:latest}
+
 set -e
 
 (set -x;
@@ -18,7 +20,7 @@ set -e
     --workdir=/chromedp \
     --env=PATH=/headless-shell \
     --env=HEADLESS_SHELL=1 \
-    chromedp/headless-shell:latest -test.v -test.parallel=1 -test.timeout=3m
+    $IMAGE -test.v -test.parallel=1 -test.timeout=3m
 )
 
 popd &> /dev/null
