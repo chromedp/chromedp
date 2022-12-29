fbdd74d278f7e2a63033efe3ea035994fafa87a9
hillguo <hillguo@tencent.com>
2022-12-29 18:08:08 +0800

    modify test examples

--- expose_test.go
+++ expose_test.go
@@ -1,9 +1,9 @@
 package chromedp
 
 import (
-	"encoding/json"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/chromedp/cdproto/runtime"
@@ -11,38 +11,15 @@ import (
 
 func TestExposeFunc(t *testing.T) {
 
-	tests := []struct {
-		name   string
-		Func   BindingFunc
-		param  string
-		result string
-	}{
-		{
-			name: "func1",
-			Func: func(param string) (string, error) {
-				return param + param, nil
-			},
-			param:  "param1",
-			result: "param1param1",
-		},
-		{
-			name: "func2",
-			Func: func(param string) (string, error) {
-				return strings.ToUpper(param), nil
-			},
-			param:  "param2",
-			result: "PARAM2",
-		},
-	}
-
 	ctx, cancel := testAllocate(t, "")
 	defer cancel()
 
-	for _, test := range tests {
-		err := ExposeFunc(ctx, test.name, test.Func)
-		if err != nil {
-			t.Fatal(err)
-		}
+	if err := ExposeFunc(ctx, "md5", func(args string) (string, error) {
+		h := md5.New()
+		h.Write([]byte(args))
+		return hex.EncodeToString(h.Sum(nil)), nil
+	}); err != nil {
+		t.Fatal(err)
 	}
 
 	if err := Run(ctx,
@@ -51,21 +28,19 @@ func TestExposeFunc(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	for _, test := range tests {
-		var res []byte
+	var res string
+	cmd := fmt.Sprintf(`%s("%s");`, "md5", "chromedp")
+	if err := Run(ctx, Evaluate(cmd, &res, func(p *runtime.EvaluateParams) *runtime.EvaluateParams {
+		return p.WithAwaitPromise(true)
+	})); err != nil {
+		t.Fatal(err)
+	}
 
-		cmd := fmt.Sprintf(`%s("%s");`, test.name, test.param)
-		err := Run(ctx, Evaluate(cmd, &res, func(p *runtime.EvaluateParams) *runtime.EvaluateParams {
-			return p.WithAwaitPromise(true)
-		}))
+	h := md5.New()
+	h.Write([]byte("chromedp"))
+	md5Str := hex.EncodeToString(h.Sum(nil))
 
-		if err != nil {
-			t.Fatal(err)
-		}
-		// When res is a *[]byte, the raw JSON-encoded value of the script result will be placed in res.
-		data, _ := json.Marshal(test.result)
-		if string(data) != string(res) {
-			t.Fatalf("want result: %s, got : %s", string(data), string(res))
-		}
+	if res != md5Str {
+		t.Fatalf("want: %s, got: %s", md5Str, res)
 	}
 }
