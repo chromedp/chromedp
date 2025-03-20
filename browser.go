eae0058f610a4cdadf1f4741c667e3b85f4ab635
David Hallenius <david@detectify.com>
2025-03-20 15:19:40 +0100

    log syntactic errors when reading messages

--- browser.go
+++ browser.go
@@ -259,6 +259,10 @@ func (b *Browser) run(ctx context.Context) {
 		for {
 			msg := new(cdproto.Message)
 			if err := b.conn.Read(ctx, msg); err != nil {
+				var syntacticError *jsontext.SyntacticError
+				if errors.As(err, &syntacticError) {
+					b.errf("%s", err)
+				}
 				return
 			}
 
